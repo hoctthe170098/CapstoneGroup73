90332d4f5ad98231615e7107bfdbe1d7f0e08be1
Anhnthe172859 <anhnthe172859@fpt.edu.vn>
2025-03-03 17:25:59 +0700

    Slot List And Create Slot

---
+++ API/src/Web/Endpoints/Slots.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyFlow.Application.Slots.Queries.GetSlots;
+using StudyFlow.Application.Common.Models;
+using StudyFlow.Application.Slots.Commands;
+
+namespace StudyFlow.Web.Endpoints;
+
+public class Slots : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapGet(GetSlots, "slots") 
+            .MapPost(CreateSlot, "slots/create"); 
+    }
+
+    public async Task<List<SlotDto>> GetSlots(ISender sender)
+    {
+        return await sender.Send(new GetSlotsQuery());
+    }
+
+    public async Task<Output> CreateSlot(ISender sender, [FromBody] CreateSlotCommand command)
+    {
+        return await sender.Send(command);
+    }
+}
