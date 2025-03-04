b76b1e7375e1fd5f7dd63bb46b3e2589d44209b2
anhhhhe170853 <133070537+haianhhg@users.noreply.github.com>
2025-03-04 22:55:54 +0700

    fix forgot password

--- UI/src/app/pages/content-pages/forgot-password/forgot-password-page.component.ts
+++ UI/src/app/pages/content-pages/forgot-password/forgot-password-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../shared/user.service';
-import { ToastrModule } from "ngx-toastr";
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-forgot-password-page',
   templateUrl: './forgot-password-page.component.html',
@@ -12,7 +12,7 @@ export class ForgotPasswordPageComponent {
   emailInvalid: boolean = false;
   message: string = '';
 
-  constructor(private authService: UserService) {}
+  constructor(private authService: UserService,private toastr: ToastrService) {}
 
   validateEmail(email: string): boolean {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -22,23 +22,29 @@ export class ForgotPasswordPageComponent {
   onSubmit(event: Event) {
     event.preventDefault();
     const emailValue = this.forgotPasswordForm.value.email;
-    
+  
     if (!emailValue || !this.validateEmail(emailValue)) {
       this.emailInvalid = true;
+      this.toastr.error('Vui lòng nhập email hợp lệ!', 'Lỗi');
       return;
     }
-    
+  
     this.emailInvalid = false;
     this.authService.forgotPassword({ email: emailValue }).subscribe({
-      next: () => {
-        this.message = 'Vui lòng kiểm tra email để đặt lại mật khẩu.';
-        this.forgotPasswordForm.reset();
+      next: (response) => {
+        if (response.code === 0 && response.message.includes("Email không khớp")) {
+          this.toastr.warning(response.message, 'Cảnh báo');
+        } else {
+          this.toastr.success('Vui lòng kiểm tra email để đặt lại mật khẩu.', 'Thành công');
+          this.forgotPasswordForm.reset();
+        }
       },
       error: () => {
-        this.message = 'Có lỗi xảy ra, vui lòng thử lại!';
+        this.toastr.error('Có lỗi xảy ra, vui lòng thử lại!', 'Lỗi');
       }
     });
   }
+  
 
   clearMessage() {
     this.message = '';
