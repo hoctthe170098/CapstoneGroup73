2a34902120d732a9df18cd40c73e71ca89ddf977
Anhnthe172859 <anhnthe172859@fpt.edu.vn>
2025-03-15 21:59:27 +0700

    FixChinhSachDto

--- API/src/Application/ChinhSachs/Queries/GetChinhSachsWithPagination/ChinhSachDto.cs
+++ API/src/Application/ChinhSachs/Queries/GetChinhSachsWithPagination/ChinhSachDto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudyFlow.Application.ChuongTrinhs.Queries.GetChuongTrinhWithPagination;
+using StudyFlow.Domain.Entities;
 
 namespace StudyFlow.Application.ChinhSachs.Queries.GetChinhSachsWithPagination;
 public class ChinhSachDto
@@ -11,4 +13,12 @@ public class ChinhSachDto
     public string Ten { get; set; } = string.Empty;
     public string Mota { get; set; } = string.Empty;
     public float PhanTramGiam { get; set; }
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<ChinhSach, ChinhSachDto>();
+            
+        }
+    }
 }
---
+++ API/src/Web/Endpoints/ChinhSachs.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyFlow.Application.ChinhSachs.Commands.CreateChinhSach;
+using StudyFlow.Application.ChinhSachs.Commands.UpdateChinhSach;
+using StudyFlow.Application.ChinhSachs.Commands.DeleteChinhSach;
+using StudyFlow.Application.ChinhSachs.Queries.GetAllChinhSachs;
+using StudyFlow.Application.ChinhSachs.Queries.GetChinhSachsWithPagination;
+using StudyFlow.Application.Common.Models;
+using StudyFlow.Domain.Constants;
+using StudyFlow.Application.ChinhSaches.Commands.CreateChinhSach;
+
+namespace StudyFlow.Web.Endpoints;
+
+public class ChinhSachs : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .DisableAntiforgery()
+            .MapPost(CreateChinhSach, "createchinhsach")
+            .MapPut(UpdateChinhSach, "updatechinhsach")
+            .MapPost(GetChinhSachsWithPagination, "getchinhsachs")
+            .MapGet(GetAllChinhSachs, "getallchinhsachs")
+            .MapDelete(DeleteChinhSach, "deletechinhsach/{id}");
+    }
+
+    //[Authorize(Roles = Roles.Administrator)]
+    public async Task<Output> CreateChinhSach(ISender sender, [FromBody] CreateChinhSachCommand command)
+    {
+        return await sender.Send(command);
+    }
+
+    //[Authorize(Roles = Roles.Administrator)]
+    public async Task<Output> UpdateChinhSach(ISender sender, [FromBody] UpdateChinhSachCommand command)
+    {
+        return await sender.Send(command);
+    }
+
+    //[Authorize(Roles = Roles.Administrator)]
+    public async Task<Output> GetChinhSachsWithPagination(ISender sender, GetChinhSachsWithPaginationQuery query)
+    {
+        return await sender.Send(query);
+    }
+
+    //[Authorize(Roles = Roles.Administrator)]
+    public async Task<Output> GetAllChinhSachs(ISender sender, [AsParameters] GetAllChinhSachsQuery query)
+    {
+        return await sender.Send(query);
+    }
+
+    //[Authorize(Roles = Roles.Administrator)]
+    public async Task<Output> DeleteChinhSach(ISender sender, string id)
+    {
+        try
+        {
+            int i_id = Int32.Parse(id);
+            return await sender.Send(new DeleteChinhSachCommand(i_id));
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
