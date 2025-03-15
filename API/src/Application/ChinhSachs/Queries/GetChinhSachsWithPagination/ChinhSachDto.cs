02fc5a383f9492965d95bba75321413a3d12c49e
Anhnthe172859 <anhnthe172859@fpt.edu.vn>
2025-03-15 17:25:37 +0700

    GetChinhSachWithPagination

---
+++ API/src/Application/ChinhSachs/Queries/GetChinhSachsWithPagination/ChinhSachDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyFlow.Application.ChinhSachs.Queries.GetChinhSachsWithPagination;
+public class ChinhSachDto
+{
+    public int Id { get; set; }
+    public string Ten { get; set; } = string.Empty;
+    public string Mota { get; set; } = string.Empty;
+    public float PhanTramGiam { get; set; }
+}
---
+++ API/src/Application/ChinhSachs/Queries/GetChinhSachsWithPagination/GetChinhSachsWithPagination.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StudyFlow.Application.Common.Exceptions;
+using StudyFlow.Application.Common.Interfaces;
+using StudyFlow.Application.Common.Mappings;
+using StudyFlow.Application.Common.Models;
+
+namespace StudyFlow.Application.ChinhSachs.Queries.GetChinhSachsWithPagination;
+
+public record GetChinhSachsWithPaginationQuery : IRequest<Output>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetChinhSachsWithPaginationQueryHandler : IRequestHandler<GetChinhSachsWithPaginationQuery, Output>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetChinhSachsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<Output> Handle(GetChinhSachsWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        if (request.PageNumber < 1 || request.PageSize < 1)
+            throw new WrongInputException("Số trang hoặc kích thước trang không hợp lệ!");
+
+        var list = await _context.ChinhSaches
+            .AsNoTracking()
+            .ProjectTo<ChinhSachDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+
+        return new Output
+        {
+            isError = false,
+            data = list,
+            code = 200,
+            message = "Lấy danh sách chính sách thành công"
+        };
+    }
+}
