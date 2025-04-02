5a6b23b0b6fa008d80f19e1fb81a070638a62a8e
Anhnthe172859 <anhnthe172859@fpt.edu.vn>
2025-04-03 00:52:13 +0700

    DeleteBaiTap

---
+++ API/src/Application/BaiTaps/Commands/DeleteBaiTap/DeleteBaiTap.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using StudyFlow.Application.Common.Exceptions;
+using StudyFlow.Application.Common.Interfaces;
+using StudyFlow.Application.Common.Models;
+using System;
+
+namespace StudyFlow.Application.BaiTaps.Commands.DeleteBaiTap;
+
+public record DeleteBaiTapCommand(Guid Id) : IRequest<Output>;
+public class DeleteBaiTapCommandHandler : IRequestHandler<DeleteBaiTapCommand, Output>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteBaiTapCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Output> Handle(DeleteBaiTapCommand request, CancellationToken cancellationToken)
+    {
+        var baiTap = await _context.BaiTaps
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (baiTap == null)
+        {
+            throw new NotFoundDataException("Không tìm thấy bài tập.");
+        }
+
+        _context.BaiTaps.Remove(baiTap);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new Output
+        {
+            isError = false,
+            code = 200,
+            data = null,
+            message = "Xóa bài tập thành công."
+        };
+    }
+}
---
+++ API/src/Application/BaiTaps/Commands/DeleteBaiTap/DeleteBaiTapCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿
+namespace StudyFlow.Application.BaiTaps.Commands.DeleteBaiTap;
+
+public class DeleteBaiTapCommandValidator : AbstractValidator<DeleteBaiTapCommand>
+{
+    public DeleteBaiTapCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id không được để trống.")
+            .Must(id => id != Guid.Empty).WithMessage("Id không hợp lệ.");
+    }
+}
