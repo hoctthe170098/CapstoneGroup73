44fbba5f3c5ec9ecbb2a4eaddc5600ed796eba4b
DungBNHE171450 <144671027+DungBNHE171450@users.noreply.github.com>
2025-04-06 16:00:15 +0700

    Fix đường dẫn ds hs

--- UI/src/app/lopdangday/danhsachhocsinh/danhsachhocsinh.component.ts
+++ UI/src/app/lopdangday/danhsachhocsinh/danhsachhocsinh.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { LopdangdayService } from '../shared/lopdangday.service';
 import { DanhSachHocSinh } from '../shared/lopdangday.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { el } from 'date-fns/locale';
 @Component({
   selector: "app-danhsachhocsinh",
   templateUrl: "./danhsachhocsinh.component.html",
@@ -14,22 +15,27 @@ export class DanhsachhocsinhComponent implements OnInit {
   constructor(
     private lopDangDayService: LopdangdayService,
     private cdr: ChangeDetectorRef,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.tenLop = this.route.parent?.snapshot.paramMap.get("tenLop");
     console.debug("Tên lớp:", this.tenLop);
-
     this.loadDanhSachHocSinh();
   }
 
   loadDanhSachHocSinh() {
     this.lopDangDayService.getDanhSachHocSinhLop(this.tenLop).subscribe(
       (response) => {
-        this.danhSachHocSinh = response.data;
-        this.cdr.detectChanges(); // Cập nhật view nếu cần thiết
-        console.debug("Danh sách học sinh:", this.danhSachHocSinh);
+        if (response.isError) {
+          this.danhSachHocSinh = response.data;
+          this.cdr.detectChanges(); // Cập nhật view nếu cần thiết
+          console.debug("Danh sách học sinh:", this.danhSachHocSinh);
+        } else {
+          if(response.code === 404)
+            this.router.navigate(['/pages/error'])
+        }
       },
       (error) => {
         console.debug("Error fetching students:", error);
