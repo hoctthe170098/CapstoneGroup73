3aafb4c73c179fc16d2d64cd7e69d484741e2bf6
hoctthe170098 <hoctthe170098@fpt.edu.vn>
2025-04-09 01:03:30 +0700

    lấy báo cáo điểm danh
    api get lấy báo cáo điểm danh

---
+++ API/src/Application/DiemDanhs/Queries/GetBaoCaoDiemDanh/BaoCaoDiemDanhDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyFlow.Application.DiemDanhs.Queries.GetBaoCaoDiemDanh
+{
+    public class BaoCaoDiemDanhDto
+    {
+        public DateOnly Ngay { get; set; }
+        public List<DiemDanhDto> DiemDanhs { get; set; } = new List<DiemDanhDto>();
+    }
+    public class DiemDanhDto
+    {
+        public string? HocSinhCode { get; set; }
+        public string? TenHocSinh { get; set; }
+        public string? TrangThai {  get; set; }
+    }
+}
