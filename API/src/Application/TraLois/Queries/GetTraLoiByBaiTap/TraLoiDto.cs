9f48cb4b832429f4897b31b1d2bb4e0f904344fa
Anhnthe172859 <anhnthe172859@fpt.edu.vn>
2025-04-09 12:49:46 +0700

    GetTraLoiByBaiTapId

---
+++ API/src/Application/TraLois/Queries/GetTraLoiByBaiTap/TraLoiDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyFlow.Application.TraLois.Queries.GetBaiTapByTraLoi;
+public class TraLoiDto
+{
+    public Guid Id { get; set; }
+    public string NoiDung { get; set; } = default!;
+    public string? UrlFile { get; set; }
+    public DateTime ThoiGian { get; set; }
+    public string HocSinhTen { get; set; } = default!;
+}
