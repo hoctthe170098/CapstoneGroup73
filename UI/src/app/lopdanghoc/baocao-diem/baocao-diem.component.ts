5a51a5c0d7f6fb1acdfdd1eb7ccb502abcb5e260
minhkienngo <kiennmhe171466@fpt.edu.vn>
2025-04-11 09:40:18 +0700

    thêm tab navigation cho lopdanghoc

---
+++ UI/src/app/lopdanghoc/baocao-diem/baocao-diem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-baocao-diem',
+  templateUrl: './baocao-diem.component.html',
+  styleUrls: ['./baocao-diem.component.scss']
+})
+export class BaocaoDiemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
